1496d313ea1180838d7e21ffe675b0f923d42a70
raspFloppy <raspfloppy@protonmail.com>
2025-09-21 22:07:21 +0200

    modified base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import solidJs from "@astrojs/solid-js"
 // https://astro.build/config
 export default defineConfig({
   site: "https://raspfloppy.github.io",
-  base: "/gabrielmonti.dev",
+  base: "/",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
